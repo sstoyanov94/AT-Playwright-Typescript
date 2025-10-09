4f6d58f2b731330ce8c902f9a0461a9163a5cc1a
sstoyanov94 <sstoyanov0@gmail.com>
2025-10-09 22:20:50 +0300

    typescript intro

---
+++ index.ts
@@ -0,0 +1,22 @@
+console.log("hello");
+
+let fName: string = '';
+
+const age: number = 20;
+
+let isTrue: boolean = 5 > 7;
+
+console.log(isTrue);
+
+let country;
+
+let lName: any = 5 > 8 
+
+let numbers: number[] = [10,20]
+let combined: (string | number | boolean)[] = ["string", 10]
+
+let isFalse = (5 > 8 && 5 > 10) === false;
+
+let or: boolean = (5 === 5 || 2 === 2)
+
+"hello" !== "hello"
